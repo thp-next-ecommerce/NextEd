dafead0211aac46ac5b30e331b0a73eb82402de3
Tim <timothee.bull@gmail.com>
2019-02-13 19:01:02 +0000

    updated basic lessons views with bootstrap

--- app/models/lesson_skill.rb
+++ app/models/lesson_skill.rb
@@ -1,5 +1,16 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: lesson_skills
+#
+#  id         :bigint(8)        not null, primary key
+#  lesson_id  :bigint(8)
+#  skill_id   :bigint(8)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class LessonSkill < ApplicationRecord
   belongs_to :lesson
   belongs_to :skill
