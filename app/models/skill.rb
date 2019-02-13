4c77db2571e42e82d7dd22800cdab0a87fa2f7a2
christopheleray <christophe.leray1@gmail.com>
2019-02-13 16:24:50 +0100

    update migration skill foreign key to domain

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -12,4 +12,5 @@
 #
 
 class Skill < ApplicationRecord
+  belongs_to :domain
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_13_141412) do
+ActiveRecord::Schema.define(version: 2019_02_13_151505) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,7 +36,10 @@ ActiveRecord::Schema.define(version: 2019_02_13_141412) do
     t.text "description", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "domain_id"
+    t.index ["domain_id"], name: "index_skills_on_domain_id"
   end
 
   add_foreign_key "domains", "cultures"
+  add_foreign_key "skills", "domains"
 end
