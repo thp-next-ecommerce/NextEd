6061244003b94eeb50ccaf9a90b96a3d0c2b4d89
christopheleray <christophe.leray1@gmail.com>
2019-02-13 17:06:44 +0100

    add culture controller

---
+++ app/controllers/cultures_controller.rb
@@ -0,0 +1,25 @@
+class CulturesController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  private
+
+  def update_params
+    @update_params ||= params.require(:culture).permit(:name, :description)
+  end
+end
