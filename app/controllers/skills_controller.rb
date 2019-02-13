1b3f94fba073043318db8758cceeb0e9e1f7d2f4
Florian <florian_pinto@outlook.fr>
2019-02-13 19:34:35 +0100

    Edit requests changes

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,25 +1,25 @@
 # frozen_string_literal: true
 
 class SkillsController < ApplicationController
+  before_action :set_skill, only: %i[show update edit]
   def index
     @skills = Skill.all
   end
 
-  def show
-    @skill = Skill.find(params[:id])
-  end
+  def show; end
 
-  def edit
-    @skill = Skill.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @skill = Skill.find(params[:id])
     @skill.update(skill_params)
     redirect_to skills_path
   end
 
   private
+
+  def set_skill
+    @skill = Skill.find(params[:id])
+  end
 
   def skill_params
     params.require(:skill).permit(:name, :description)
