f7a33ec06996cd830ae0bec976577e9dbf28ab65
Tim <timothee.bull@gmail.com>
2019-02-19 11:39:13 +0100

    annotate gem did its job

--- spec/factories/students.rb
+++ spec/factories/students.rb
@@ -1,5 +1,16 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: students
+#
+#  id         :bigint(8)        not null, primary key
+#  first_name :string
+#  last_name  :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 FactoryBot.define do
   factory :student do
     first_name { "MyString" }
--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -1,5 +1,16 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: students
+#
+#  id         :bigint(8)        not null, primary key
+#  first_name :string
+#  last_name  :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'rails_helper'
 
 RSpec.describe Student, type: :model do
