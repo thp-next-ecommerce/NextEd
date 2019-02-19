7bfd4cba8f9b71a60f254201ec232f4ab3257e3d
Ronan BORGNE <borgne.ronan@gmail.com>
2019-02-19 16:38:40 +0100

    has_many through student et work_session

--- app/models/student.rb
+++ app/models/student.rb
@@ -14,4 +14,6 @@
 class Student < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
+  has_many :student_work_sessions, dependent: :destroy
+  has_many :work_sessions, through: :student_work_sessions
 end
