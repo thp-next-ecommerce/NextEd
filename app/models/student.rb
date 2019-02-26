1b63e0ef5c52104f231a5361f17bef313da0563c
Tim <timothee.bull@gmail.com>
2019-02-26 15:41:44 +0100

    commented student search logic in related files

--- app/models/student.rb
+++ app/models/student.rb
@@ -20,5 +20,5 @@ class Student < ApplicationRecord
   has_many :groups, through: :group_students
   has_many :student_work_sessions, dependent: :destroy
   has_many :work_sessions, through: :student_work_sessions
-  paginates_per 25
+  # paginates_per 25
 end
