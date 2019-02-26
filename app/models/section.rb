32ea962234c791f3f746b4083ffdda2ec2ab4222
Juliette Audema <juliette.audema@gmail.com>
2019-02-26 18:48:24 +0100

    Correct tests

--- app/models/section.rb
+++ app/models/section.rb
@@ -5,11 +5,12 @@
 # Table name: sections
 #
 #  id          :bigint(8)        not null, primary key
-#  year        :integer          not null
+#  year_start  :integer          not null
 #  level       :integer          not null
 #  sub_section :string           not null
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
+#  year_end    :integer          not null
 #
 
 class Section < ApplicationRecord
--- spec/factories/scholar_years.rb
+++ spec/factories/scholar_years.rb
@@ -1,5 +1,17 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: scholar_years
+#
+#  id         :bigint(8)        not null, primary key
+#  start_date :date
+#  end_date   :date
+#  current    :boolean          default(FALSE)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 FactoryBot.define do
   factory :scholar_year do
     year_start { "" }
