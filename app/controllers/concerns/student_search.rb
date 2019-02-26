c7fd333ae548f1c0db29982f57bf3a85581a14be
Tim <timothee.bull@gmail.com>
2019-02-26 11:20:40 +0100

    extracted student search feature in a concern

---
+++ app/controllers/concerns/student_search.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+module StudentSearch
+  extend ActiveSupport::Concern
+
+  def search
+    if params[:level].present? && params[:sub_section].present?
+      @students = Student.select { |student|
+        (student.sections.last.sub_section == params[:sub_section] &&
+        student.sections.last.level == params[:level].to_i)
+      }
+
+    elsif params[:group].present?
+      if params[:group] == "yes"
+        @groups = Group.select { |group|
+          (group.level == params[:level].to_i)
+        }
+      else
+        @group = Group.find(params[:group])
+        @students = @group.students
+      end
+
+    elsif params[:student].present?
+      @students = Student.select { |student|
+        (student.last_name.upcase.start_with?(params[:student].upcase) ||
+        student.first_name.upcase.start_with?(params[:student].upcase) )
+      }
+    end
+  end
+end
