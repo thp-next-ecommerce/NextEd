9ebc8842eca0c456d03e9289e4b59f089bc54205
Ronan BORGNE <borgne.ronan@gmail.com>
2019-02-27 17:48:30 +0100

    annotate

--- spec/factories/sections.rb
+++ spec/factories/sections.rb
@@ -1,16 +1,14 @@
 # frozen_string_literal: true
-
 # == Schema Information
 #
 # Table name: sections
 #
 #  id          :bigint(8)        not null, primary key
-#  year_start  :integer          not null
+#  year        :integer          not null
 #  level       :integer          not null
 #  sub_section :string           not null
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
-#  year_end    :integer          not null
 #
 
 FactoryBot.define do
