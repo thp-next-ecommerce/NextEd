511344bb204675050098c05c71e6c02e3d2ba4c0
christopheleray <christophe.leray1@gmail.com>
2019-02-28 08:31:11 +0100

    rspec model student_work_session

--- spec/factories/student_work_sessions.rb
+++ spec/factories/student_work_sessions.rb
@@ -13,8 +13,8 @@
 
 FactoryBot.define do
   factory :student_work_session do
-    student { nil }
-    work_session { nil }
+    student
+    work_session
     present { false }
   end
 end
--- spec/models/student_work_session_spec.rb
+++ spec/models/student_work_session_spec.rb
@@ -14,5 +14,25 @@
 require 'rails_helper'
 
 RSpec.describe StudentWorkSession, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'model instanciation' do
+    describe 'database' do
+      it { is_expected.to have_db_column(:present).of_type(:boolean).with_options(default: false)}
+      it { is_expected.to have_db_column(:student_id).of_type(:integer)}
+      it { is_expected.to have_db_column(:work_session_id).of_type(:integer)}
+    end
+
+    describe 'asscociations' do 
+      let(:student_work_session) { create(:student_work_session) }
+      it { is_expected.to belong_to(:student) }
+      it { is_expected.to belong_to(:work_session) }
+
+      it 'responds to #students' do
+        expect(student_work_session.student).to be_truthy
+      end
+
+      it 'responds to #work_session' do
+        expect(student_work_session.work_session).to be_truthy
+      end
+    end
+  end
 end
