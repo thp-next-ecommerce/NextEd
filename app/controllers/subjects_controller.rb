e80c366bac0df43f056047dc561dff398677be6c
Ronan BORGNE <borgne.ronan@gmail.com>
2019-03-05 21:28:50 +0100

    scaffolded subject

---
+++ app/controllers/subjects_controller.rb
@@ -0,0 +1,59 @@
+class SubjectsController < ApplicationController
+  before_action :set_subject, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @subjects = Subject.all
+  end
+
+  def show; end
+
+  def new
+    @subject = Subject.new
+  end
+
+  def edit; end
+
+  def create
+    @subject = Subject.new(subject_params)
+
+    respond_to do |format|
+      if @subject.save
+        format.html { redirect_to @subject, notice: 'Subject was successfully created.' }
+        format.json { render :show, status: :created, location: @subject }
+      else
+        format.html { render :new }
+        format.json { render json: @subject.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @subject.update(subject_params)
+        format.html { redirect_to @subject, notice: 'Subject was successfully updated.' }
+        format.json { render :show, status: :ok, location: @subject }
+      else
+        format.html { render :edit }
+        format.json { render json: @subject.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @subject.destroy
+    respond_to do |format|
+      format.html { redirect_to subjects_url, notice: 'Subject was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+  
+  def set_subject
+    @subject = Subject.find(params[:id])
+  end
+  
+  def subject_params
+    params.require(:subject).permit(:name, :description)
+  end
+end
