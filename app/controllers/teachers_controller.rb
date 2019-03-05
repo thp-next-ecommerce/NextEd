df9b1719990f36a9efe5e054fe1d44808c4b48ab
Ronan BORGNE <borgne.ronan@gmail.com>
2019-03-06 00:41:42 +0100

    scaffolded teacher

---
+++ app/controllers/teachers_controller.rb
@@ -0,0 +1,61 @@
+# frozen_string_literal: true
+
+class TeachersController < ApplicationController
+  before_action :set_teacher, only: %i[show edit update destroy]
+
+  def index
+    @teachers = Teacher.all
+  end
+
+  def show; end
+
+  def new
+    @teacher = Teacher.new
+  end
+
+  def edit; end
+
+  def create
+    @teacher = Teacher.new(teacher_params)
+
+    respond_to do |format|
+      if @teacher.save
+        format.html { redirect_to @teacher, notice: 'Teacher was successfully created.' }
+        format.json { render :show, status: :created, location: @teacher }
+      else
+        format.html { render :new }
+        format.json { render json: @teacher.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @teacher.update(teacher_params)
+        format.html { redirect_to @teacher, notice: 'Teacher was successfully updated.' }
+        format.json { render :show, status: :ok, location: @teacher }
+      else
+        format.html { render :edit }
+        format.json { render json: @teacher.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @teacher.destroy
+    respond_to do |format|
+      format.html { redirect_to teachers_url, notice: 'Teacher was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+  def set_teacher
+    @teacher = Teacher.find(params[:id])
+  end
+
+  def teacher_params
+    params.require(:teacher).permit(:first_name, :last_name)
+  end
+end
