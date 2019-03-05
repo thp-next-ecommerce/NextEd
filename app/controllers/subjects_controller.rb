1dccd43e3bdfabed548cf6c7659d10692f247750
Ronan BORGNE <borgne.ronan@gmail.com>
2019-03-05 21:30:48 +0100

    rubocop

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class SubjectsController < ApplicationController
-  before_action :set_subject, only: [:show, :edit, :update, :destroy]
+  before_action :set_subject, only: %i[show edit update destroy]
 
   def index
     @subjects = Subject.all
@@ -48,11 +50,11 @@ class SubjectsController < ApplicationController
   end
 
   private
-  
+
   def set_subject
     @subject = Subject.find(params[:id])
   end
-  
+
   def subject_params
     params.require(:subject).permit(:name, :description)
   end
