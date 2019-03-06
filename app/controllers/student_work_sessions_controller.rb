1afdc76ae9238cc7cbbead165baf5a44b6cfda53
christopheleray <christophe.leray1@gmail.com>
2019-03-06 14:32:20 +0100

    update roll-call-controller and views

--- app/controllers/student_work_sessions_controller.rb
+++ app/controllers/student_work_sessions_controller.rb
@@ -2,14 +2,16 @@
 
 class StudentWorkSessionsController < ApplicationController
   def roll_call
-    @student_work_sessions = StudentWorkSession.where(work_session_id: params[:id]).not_attended
-    @students_attended = StudentWorkSession.where(work_session_id: params[:id]).has_attended
+    @student_work_sessions = StudentWorkSession.where(work_session_id: params[:id]).has_attended
   end
 
   def update
     if params["student_work_session"].present?
       # display an error if form is validated without students selection
-      @student_work_sessions = StudentWorkSession.find(params["student_work_session"]["attended"])
+      @student_work_sessions = StudentWorkSession.find(params["student_work_session"]["attended"] ||
+                                                       params["student_work_session"]["later"] ||
+                                                       params["student_work_session"]["suspended"] || 
+                                                       params["student_work_session"]["medical"])
       @student_work_sessions.each do |student|
         unless student.update(update_params)
           redirect_back fallback_location: work_sessions_path, alert: @student_work_sessions.errors.full_messages.to_s
@@ -24,6 +26,6 @@ class StudentWorkSessionsController < ApplicationController
   private
 
   def update_params
-    params.require(:student_work_session).permit(attended: [])
+    params.require(:student_work_session).permit(attended: [], later: [], medical: [], suspended: [])
   end
 end
