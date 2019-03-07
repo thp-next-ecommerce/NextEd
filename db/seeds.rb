0bf30cc4fefb29710bd09962970e21c83b0c403f
Tim <timothee.bull@gmail.com>
2019-03-07 08:22:14 +0000

    more work_sessions created in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -60,7 +60,7 @@ puts "Created domains"
 end
 puts "created 10 Skills"
 
-10.times do
+30.times do
   schedule = %w(M1 M2 M3 M4 S1 S2 S3 S4)
   skills = []
   5.times do skills.push(Skill.all.sample) end
