84a36e8127b1f8445a4878955660513291073869
Tim <timothee.bull@gmail.com>
2019-03-11 13:53:49 +0000

    cleaned students_controller#show

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -13,13 +13,6 @@ class StudentsController < ApplicationController
   # GET /students/1
   # GET /students/1.json
   def show
-    # get all studied skills
-    # skills = @student.student_work_sessions.has_attended.map { |sws|
-    #  sws.work_session.skills
-    # }.flatten
-    # classify all skills in a [<skill>, <study_count>] format
-    # @skills_recap = skills.each_with_object(Hash.new(0)) { |o, h| h[o] += 1 } if skills
-
     @skills = @student.skills_with_sections
   end
 
