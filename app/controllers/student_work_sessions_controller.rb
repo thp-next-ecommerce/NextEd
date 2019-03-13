c4e96e4791119269d8bea760c7cd009335b0d803
christopheleray <christophe.leray1@gmail.com>
2019-03-13 12:35:48 +0100

    change methode name is_present to params_present?

--- app/controllers/student_work_sessions_controller.rb
+++ app/controllers/student_work_sessions_controller.rb
@@ -6,14 +6,14 @@ class StudentWorkSessionsController < ApplicationController
   end
 
   def update
-    update_attendance("absent", "attended", false) if is_present?("absent")
-    update_attendance("present", "attended") if is_present?("present")
-    update_attendance("late") if is_present?("late")
-    update_attendance("medical") if is_present?("medical")
-    update_attendance("suspended") if is_present?("suspended")
-    update_attendance("!late", "late", false) if is_present?("!late")
-    update_attendance("!medical", "medical", false) if is_present?("!medical")
-    update_attendance("!suspended", "suspended", false) if is_present?("!suspended")
+    update_attendance("absent", "attended", false) if params_present?("absent")
+    update_attendance("present", "attended") if params_present?("present")
+    update_attendance("late") if params_present?("late")
+    update_attendance("medical") if params_present?("medical")
+    update_attendance("suspended") if params_present?("suspended")
+    update_attendance("!late", "late", false) if params_present?("!late")
+    update_attendance("!medical", "medical", false) if params_present?("!medical")
+    update_attendance("!suspended", "suspended", false) if params_present?("!suspended")
 
     UpdateSkillStudentsJob.perform_later(params[:id])
     redirect_to work_session_path(params[:id])
@@ -33,7 +33,7 @@ class StudentWorkSessionsController < ApplicationController
     end
   end
 
-  def is_present?(params_key)
+  def params_present?(params_key)
     params["student_work_session"][params_key]
   end
 end
