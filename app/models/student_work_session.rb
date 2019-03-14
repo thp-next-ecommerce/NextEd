09565130b24784700ec56fb52cad5ced091aa96a
christopheleray <christophe.leray1@gmail.com>
2019-03-14 14:35:11 +0100

    commit backup modifiction model validation and views

--- app/models/student_work_session.rb
+++ app/models/student_work_session.rb
@@ -16,9 +16,26 @@
 #
 
 class StudentWorkSession < ApplicationRecord
+  before_update :update_attended_status_if_needed
   belongs_to :student
   belongs_to :work_session
 
   scope :has_attended, lambda { where(attended: true) }
   scope :not_attended, lambda { where(attended: false) }
+  #validate :check_student_status
+
+  def update_attended_status_if_needed
+    self.attended = true if self.late
+    self.attended = false if self.suspended
+  end
+
+  def check_student_status
+
+    if self.late == self.medical || self.late == self.suspended || self.suspended == self.medical
+      errors.add(:base, "Sélectionner une seul options: en retard, nurserie ou exclus" )
+      puts "---------"
+      puts self.errors.full_messages
+    end
+  end
+
 end
