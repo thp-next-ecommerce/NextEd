9cfc735b386f06a7fe23d9329f14583548a2cdab
Tim <timothee.bull@gmail.com>
2019-03-14 16:25:55 +0000

    annotate gem did its job

--- app/models/import_error.rb
+++ app/models/import_error.rb
@@ -1,5 +1,18 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: import_errors
+#
+#  id            :bigint(8)        not null, primary key
+#  error_entries :string           not null
+#  line          :string           not null
+#  error_type    :string           not null
+#  data_type     :string           not null
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
+
 class ImportError < ApplicationRecord
   serialize :error_entries, Array
 end
--- spec/factories/import_errors.rb
+++ spec/factories/import_errors.rb
@@ -1,5 +1,18 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: import_errors
+#
+#  id            :bigint(8)        not null, primary key
+#  error_entries :string           not null
+#  line          :string           not null
+#  error_type    :string           not null
+#  data_type     :string           not null
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
+
 FactoryBot.define do
   factory :import_error do
     error_entries { ["MyString"] }
